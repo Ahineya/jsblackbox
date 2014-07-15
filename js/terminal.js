970518359a134e0ca45e1252bec8b14fb277d8cc
Pavel Evsegneev <pavel@evsegneev.pp.ua>
2014-07-15 13:01:27 +0300

    Terminal created

--- js/terminal.js
+++ js/terminal.js
@@ -1,5 +1,69 @@
-function Terminal() {
-    return this;
+function Terminal(invalues, validvalues) {
+
+    this.inValues = invalues || [1,2,3];
+    this.validValues = validvalues || [1,2,3];
+    this.program = '(function() {return 1;})';
+    this.status = false;
+
+    var self = this;
+
+    this._get = function() {
+        return {
+            input: self.inValues,
+            valid: self.validValues
+        };
+    };
+
+    this._program = function(program) {
+        if (program !== '') {
+            self.program = program;
+            return true;
+        }
+
+        return false;
+    };
+
+    this._status = function() {
+        return self.status;
+    };
+
+    this._process = function() {
+        try {
+            var func = eval(self.program); // jshint ignore:line
+
+            var result = [];
+
+            var status = true;
+
+            for (var i = 0; i < self.inValues.length; i++) {
+                var value = func(self.inValues[i]);
+                var st = value === self.validValues[i];
+                result.push({
+                    value: value,
+                    passed: st
+                });
+
+                if (!st) {
+                    status = false;
+                }
+
+            }
+
+            self.status = status;
+
+            return result;
+
+        } catch (e) {
+            return e.toString();
+        }
+    };
+
+    return {
+        get: this._get,
+        program: this._program,
+        status: this._status,
+        process: this._process
+    };
 }
 
 if ((typeof module) !== 'undefined') {
--- test/test.js
+++ test/test.js
@@ -2,8 +2,57 @@ var chai = require('chai');
 var Terminal = require('../js/terminal.js').Terminal;
 
 describe('1 Terminal', function() {
-    var t = new Terminal();
+    var t = new Terminal([1,2,3], [1,2,3]);
     it('1.1 Should create terminal object', function() {
-        chai.assert((t instanceof Terminal), "Didn't create Terminal instance");
+        chai.assert((typeof t !== 'undefined'), "Didn't create Terminal instance");
     });
+
+    it('1.2 should initialize with data', function() {
+        chai.assert(t.get().input[0] == 1, "Input did't set correctly");
+        chai.assert(t.get().input[1] == 2, "Input did't set correctly");
+        chai.assert(t.get().input[2] == 3, "Input did't set correctly");
+
+        chai.assert(t.get().valid[0] == 1, "Valid did't set correctly");
+        chai.assert(t.get().valid[1] == 2, "Valid did't set correctly");
+        chai.assert(t.get().valid[2] == 3, "Valid did't set correctly");
+    });
+
+    it('1.3 should be programmed', function() {
+        chai.assert(t.program('(function(num) {return 1;})'), 'Terminal was not programmed');
+    });
+
+    it('1.4 should have false status before processing', function() {
+        chai.assert(!t.status(), 'Status is incorrect');
+    });
+
+    it('1.5 should process incorrect program', function() {
+        var values = t.process(); //[{value: 1, passed: true},{value: 1, passed: false},{value: 1, passed: false}]
+
+        chai.assert(values[0].passed && (values[0].value === 1), "Incorrect processing");
+        chai.assert(!values[1].passed && (values[1].value === 1), "Incorrect processing");
+        chai.assert(!values[2].passed && (values[2].value === 1), "Incorrect processing");
+
+    });
+
+    it('1.6 should have false status after processing incorrect program', function() {
+        chai.assert(!t.status(), 'Status is incorrect');
+    });
+
+    it('1.7 should be programmed again', function() {
+        chai.assert(t.program('(function(num) {return num;})'), 'Terminal was not programmed');
+    });
+
+    it('1.8 should process correct program', function() {
+        var values = t.process(); //[{value: 1, passed: true},{value: 2, passed: true},{value: 3, passed: true}]
+
+        chai.assert(values[0].passed && (values[0].value === 1), "Incorrect processing");
+        chai.assert(values[1].passed && (values[1].value === 2), "Incorrect processing");
+        chai.assert(values[2].passed && (values[2].value === 3), "Incorrect processing");
+
+    });
+
+    it('1.9 should have true status after processing incorrect program', function() {
+        chai.assert(t.status(), 'Status is incorrect');
+    });
+
 });
