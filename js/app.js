792fe0ebbac5e5034b22de625168410fb46cb37d
= <deadswallow@gmail.com>
2014-07-16 23:23:57 +0300

    Finishing bug fix

--- js/app.js
+++ js/app.js
@@ -176,7 +176,7 @@
 
         $('.next').off('click').on('click', function () {
             currentLevel++;
-            if (levelsArr.indexOf(currentLevel) > -1) {
+            if (levelsArr[currentLevel !== 'undefined']) {
                 loadLevel(levelsArr[currentLevel]);
             } else {
                 toggleContainer('hide');
